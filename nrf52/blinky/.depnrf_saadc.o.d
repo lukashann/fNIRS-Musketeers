b3dd7c070e8a362d10d74985706b22c67daf3674
Lukas Hann <lukas.hann95@gmail.com>
2017-01-27 16:02:40 +0000

    Added nRF52. contains linker and makefile and an empty project (blinky) that will be completed soon

---
+++ nrf52/blinky/.depnrf_saadc.o.d
@@ -0,0 +1,5 @@
+../sdk/nRF5_SDK_12.2.0_f012efa/components/drivers_nrf/hal/nrf_saadc.o: \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/drivers_nrf/hal/nrf_saadc.c \
+ include/sdk_config.h
+
+include/sdk_config.h:
---
+++ nrf52/blinky/.depnrf_soc.o.d
@@ -0,0 +1,107 @@
+../sdk/nRF5_SDK_12.2.0_f012efa/components/drivers_nrf/nrf_soc_nosd/nrf_soc.o: \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/drivers_nrf/nrf_soc_nosd/nrf_soc.c \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/stdlib.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/machine/ieeefp.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/_ansi.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/newlib.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/_newlib_version.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/config.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/features.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/lib/gcc/arm-none-eabi/6.2.1/include/stddef.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/reent.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/_ansi.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/_types.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/machine/_types.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/machine/_default_types.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/lock.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/cdefs.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/machine/stdlib.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/alloca.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/drivers_nrf/nrf_soc_nosd/nrf_soc.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/lib/gcc/arm-none-eabi/6.2.1/include/stdint.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/stdint.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/_intsup.h \
+ /Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/_stdint.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/device/nrf.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/device/nrf52.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cm4.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmInstr.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmFunc.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmSimd.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/system_nrf52.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/device/nrf52_bitfields.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/device/nrf51_to_nrf52.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/device/nrf52_name_change.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/device/compiler_abstraction.h \
+ ../sdk/nRF5_SDK_12.2.0_f012efa/components/drivers_nrf/nrf_soc_nosd/nrf_error.h
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/stdlib.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/machine/ieeefp.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/_ansi.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/newlib.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/_newlib_version.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/config.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/features.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/lib/gcc/arm-none-eabi/6.2.1/include/stddef.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/reent.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/_ansi.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/_types.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/machine/_types.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/machine/_default_types.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/lock.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/cdefs.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/machine/stdlib.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/alloca.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/drivers_nrf/nrf_soc_nosd/nrf_soc.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/lib/gcc/arm-none-eabi/6.2.1/include/stdint.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/stdint.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/_intsup.h:
+
+/Users/lukashann/Documents/Imperial\ College\ London/Year\ 3/Project/fNIRS-Musketeers/nrf52/toolchain/arm_cm4/arm-none-eabi/include/sys/_stdint.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/device/nrf.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/device/nrf52.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cm4.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmInstr.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmFunc.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmSimd.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/toolchain/system_nrf52.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/device/nrf52_bitfields.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/device/nrf51_to_nrf52.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/device/nrf52_name_change.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/device/compiler_abstraction.h:
+
+../sdk/nRF5_SDK_12.2.0_f012efa/components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
